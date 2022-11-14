d1314c01462a6bab2b5a51f82793d787d58dfaec
OnionPowder01 <autonatione36@gmail.com>
2022-11-14 11:49:36 +0200

    ClickEventsModule

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import { initialdomManip } from "./Initial-dom-manipulation";
+import { displayDefaultProject, displayTheForm, addItemToCheckList, clearForm } from "./dom-manipulation";
 import { blankProjectLoad } from "./blank-project-load";
 import { createToDo } from "./create-to-do";
   
 // Call blankProjectLoad on first 
 blankProjectLoad();
 
-//Call DOM Manipulation module for UI
-initialdomManip();
 
-//TODO: Click event module here for todo and project creations
+
+//Event Module
+let clickEventsModule = (function() {
+
+    //Displaying the form
+    const addNewToDo = document.querySelector(".add-todo-button")
+    addNewToDo.addEventListener("click", displayTheForm()) 
+
+    //Add item to the checklist of the form
+    const addToCheckList = document.querySelector(".add-to-checklist");
+    addToCheckList.addEventListener("click", addItemToCheckList()) ;
+
+    //Clear the form
+    const clearButton = document.querySelector(".reset-button");
+    clearButton.addEventListener("click", clearForm()); 
+})();
+
 
 //create-to-do.js module file and apply some objects properties
 let myToDo = createToDo("Grocery Run", "Go get groceries", "15/11/2022", "Low", "Meat, Eggs, Milk");
