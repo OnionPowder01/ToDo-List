cf8e6cff171098e069663bbcfddd57f115ebf156
OnionPowder01 <autonatione36@gmail.com>
2022-11-15 11:36:03 +0200

    style loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,18 @@
-const { NONAME } = require('dns');
-const path = require('path');
+const path = require("path");
 
-module.exports = 
-{ 
-  mode: 'production',
-  entry: './src/index.js',
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  
-      module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-         {
-           test: /\.(png|svg|jpg|jpeg|gif)$/i,
-           type: 'asset/resource',
-         },
-         {
-          test: /\.(scss)$/,
-          use: [{
-            loader: 'style-loader', // inject CSS to page
-          }, {
-            loader: 'css-loader', // translates CSS into CommonJS modules
-          }, {
-            loader: 'postcss-loader', // Run post css actions
-            options: {
-              plugins: function () { // post css plugins, can be exported to postcss.config.js
-                return [
-                  require('precss'),
-                  require('autoprefixer')
-                ];
-              }
-            }
-          }, {
-            loader: 'sass-loader' // compiles Sass to CSS
-          }]
-        },
-        ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
-        
+    ],
+  },
 };
