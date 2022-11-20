bf7c8f4a6d770eba9d49027f9ef8ec1f2e3538b3
OnionPowder01 <autonatione36@gmail.com>
2022-11-20 21:57:09 +0200

    Improve

--- src/create-to-do.js
+++ src/create-to-do.js
@@ -1,19 +1,19 @@
 import { compareAsc, format, parseISO, startOfToday } from "date-fns";
 import { clearForm } from "./dom-manipulation.js";
-import { savetoDoLocal } from "./local-storage.js";
+import { saveToDoLocal } from "./local-storage.js";
 
 //Creating an array for ToDo if needed
 let toDoArray = [];
 
 //Factory function to create ToDo list
 export const createToDo = () => {
-  let title = document.getElementById("Title").value;
-  let description = document.getElementById("Description").value;
-  let dueDate = document.getElementById("DueDate").value;
-  let priority = document.getElementById("Priority").value;
+  let Title = document.getElementById("Title").value;
+  let Description = document.getElementById("Description").value;
+  let DueDate = document.getElementById("DueDate").value;
+  let Priority = document.getElementById("Priority").value;
 
   //Check to see if empty fields
-  if (title == "" || description == " " || dueDate == "") {
+  if (Title == "" || Description == " " || DueDate == "") {
     alert(
       "Title, Descritpion and Due Date are required fields, please try again!"
     );
@@ -21,18 +21,19 @@ export const createToDo = () => {
   }
 
   //Check to see if pre-date entered
-  if (parseISO(dueDate) < startOfToday()) {
+  if (parseISO(DueDate) < startOfToday()) {
     alert(
       "You have entered a date that has already passed! Please enter a date greater than or equal to today. "
     );
-    console.log("due date", parseIso(dueDate));
+    console.log("due date", parseIso(DueDate));
     console.log("date now", startOfToday());
     return;
   }
   //Loop over the nodelist for check list items from the DOM
-  const nodeListCheckList = document.querySelectorAll("li");
+  const nodeListCheckList = document.querySelectorAll("form.li");
   let checkListArray = [];
   for (let i = 0; i < nodeListCheckList.length; i++) {
+
     //push the "x" to temp array
     let strippedCheckList = nodeListCheckList[i].textContent.replace(
       "\u00D7",
@@ -41,20 +42,21 @@ export const createToDo = () => {
     checkListArray.push(strippedCheckList);
   }
 
-  let checklist = checkListArray.join(", ");
+  let Checklist = checkListArray.join(", ");
 
   console.log("Called createToDo module.. creating todo now");
-  console.log({ title, description, dueDate, priority, checklist });
+  console.log({ Title, Description, DueDate, Priority, Checklist });
   console.log("Pushing this object to the toDoArray");
 
-  toDoArray.push({ title, description, dueDate, priority, checklist });
+  toDoArray.push({ Title, Description, DueDate, Priority, Checklist });
   console.log(toDoArray);
 
+  saveToDoLocal({ Title, Description, DueDate, Priority}, Checklist);
+  
   //Reset the form after succesful submission
   clearForm();
 
   //Storage module for pushing to local storage
-  savetoDoLocal({ title, description, dueDate, priority, checklist });
-
-  return { title, description, dueDate, priority, checklist };
+  return ({ Title, Description, DueDate, Priority}, Checklist);
+  
 };
